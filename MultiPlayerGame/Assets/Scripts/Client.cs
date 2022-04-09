fef1004a8d11c7d4a830fc858bf9b1a9bdfcb496
[Dev-BaeJS] <[aqowjdtna@ajou.ac.kr]>
2022-04-09 17:13:31 +0900

    tutorial Step 7

--- MultiPlayerGame/Assets/Scripts/Client.cs
+++ MultiPlayerGame/Assets/Scripts/Client.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using System.Net;
 using System.Net.Sockets;
 using System;
--- MultiPlayerGame/Assets/Scripts/Packet.cs
+++ MultiPlayerGame/Assets/Scripts/Packet.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEditor;
 
 /// <summary>Sent from server to client.</summary>
 public enum ServerPackets
